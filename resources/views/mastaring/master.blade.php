994328d9bc5332de5db5e6e1f1f9bc2ae43afec2
asraf-mridha <asrafmridha@gmail.com>
2022-08-30 09:07:52 +0600

    Mastaring Template Successfully

---
+++ resources/views/mastaring/master.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  @include('backend.includes.header')
+
+  @include('backend.includes.css')
+  
+  <body>
+    
+      @include('backend.includes.leftbar')
+
+      @include('backend.includes.headbar')
+
+      @include('backend.includes.rightbar')
+
+      
+
+       <!-- ########## START: MAIN PANEL ########## -->
+    <div class="br-mainpanel">
+      <div class="br-pagetitle">
+        <i class="icon ion-ios-home-outline"></i>
+        <div>
+          <h4>Dashboard</h4>
+          <p class="mg-b-0">Do bigger things with Bracket plus, the responsive bootstrap 4 admin template.</p>
+        </div>
+      </div>
+
+      <div class="br-pagebody">
+        <div class="row row-sm">
+          @yield('content')
+      </div><!-- row -->
+
+      </div><!-- br-pagebody -->
+      @include('backend.includes.footer')
+    </div><!-- br-mainpanel -->
+    <!-- ########## END: MAIN PANEL ########## -->
+    @include('backend.includes.script')
+    
+
+   
+  </body>
+</html>
