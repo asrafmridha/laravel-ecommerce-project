5c6073bc8255c3f0dd2d9a56e4f458b4779f142f
asraf-mridha <asrafmridha@gmail.com>
2022-09-02 23:16:52 +0600

    Crate field in Products table

--- database/migrations/2022_09_02_165439_create_products_table.php
+++ database/migrations/2022_09_02_165439_create_products_table.php
@@ -14,7 +14,21 @@ return new class extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->integer('vendor_id');
+            $table->integer('cat_id');
+            $table->integer('subcat_id');
+            $table->string('product_name');
+            $table->string('slug');
+            $table->integer('product_code');
+            $table->float('product_price');
+            $table->float('discount');
+            $table->float('discount_price');
+            $table->text('short_description');
+            $table->text('long_description');
+            $table->string('thumbnails');
+            $table->integer('quantity');
+            $table->integer('status');
             $table->timestamps();
         });
     }
