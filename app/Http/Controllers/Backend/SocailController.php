299db87bcc9a001131545f99da6dfa37e52a3b6c
asraf-mridha <asrafmridha@gmail.com>
2022-09-07 23:48:15 +0600

    Write Condition for social login in SocialController

--- app/Http/Controllers/Backend/SocailController.php
+++ app/Http/Controllers/Backend/SocailController.php
@@ -4,8 +4,20 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
 
 class SocailController extends Controller
 {
+
+    public function gotogoogle(){
+
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function googlestore(){
+
+        $user = Socialite::driver('google')->user();
+        dd($user);
+    }
     
 }
