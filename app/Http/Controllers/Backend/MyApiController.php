b57a18349b05f919b9fb86a81011d827add70371
asraf-mridha <asrafmridha@gmail.com>
2022-09-08 22:00:15 +0600

    Add Token for data save in MyApiController

--- app/Http/Controllers/Backend/MyApiController.php
+++ app/Http/Controllers/Backend/MyApiController.php
@@ -3,9 +3,54 @@
 namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Backend\Subcategory;
 use Illuminate\Http\Request;
 
 class MyApiController extends Controller
 {
-    //
+   public function alldta(){
+
+    $alldata= Subcategory::all();
+
+    return response()->json([
+
+         'alldata'=>$alldata
+
+    ]);
+   }
+
+   public function store(Request $request){
+        
+      if($request->token=='asraf1234'){
+        $subcategory= new Subcategory;
+        $subcategory->cat_id=$request->cat_id;
+        $subcategory->slug=$request->slug;
+        $subcategory->name=$request->name;
+        $subcategory->status=$request->status;
+        $subcategory->save();
+
+        if($subcategory->save()){
+            return response()->json([
+
+                'status'=>'success',
+
+            ]);
+        }
+      }
+
+        else{
+
+            return response()->json([
+
+                'status'=>'404',
+
+            ]);
+
+
+        }
+        
+
+
+
+   }
 }
