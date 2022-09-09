d4e9fb4aa49b02a64e033e8db9f1b190e8422a5b
asraf-mridha <asrafmridha@gmail.com>
2022-09-09 23:35:48 +0600

    Create function in Controller for store data in database

--- app/Http/Controllers/Backend/ApiUserController.php
+++ app/Http/Controllers/Backend/ApiUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Backend\Apiuser;
 use Illuminate\Http\Request;
 
 class ApiUserController extends Controller
@@ -10,4 +11,19 @@ class ApiUserController extends Controller
      public function add(){
         return view('api/add');
      }  
+
+     public function adduser(Request $request){
+
+        $apiuser= new Apiuser;
+        $apiuser->name=$request->uname;
+        $apiuser->address=$request->address;
+        $apiuser->phone=$request->phone;
+        $apiuser->email=$request->email;
+        $apiuser->nid=$request->nid;
+        $apiuser->save();
+
+
+
+
+     }
 }
