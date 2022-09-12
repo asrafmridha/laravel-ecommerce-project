7b1c6ba5ade5bdbec47d49885c0aa52b41e2ef62
asraf-mridha <asrafmridha@gmail.com>
2022-09-12 23:27:56 +0600

    Make validation message if user give null value

--- resources/views/backend/pages/slider/addsliderview.blade.php
+++ resources/views/backend/pages/slider/addsliderview.blade.php
@@ -4,44 +4,78 @@
 
  <div class="col-md-6">
 
+    @if(Session::has('message'))
+   <p class="alert alert-info">{{ Session::get('message') }}</p>
+   @endif
+
     <form action="{{Route('sliderstore')}}"  method="POST" enctype="multipart/form-data"
     >
 
         @csrf
 
-  </select>
-  <span class="error_name"></span>
+        @error('category')
+        <div class="alert alert-danger">
+
+            {{$message}}
+        </div>  
+        @enderror
+
   <input type="text" name="category" class="mt-3 form-control name" placeholder="Enter Category name">
 
+  @error('title')
+  <div class="alert alert-danger">
 
+      {{$message}}
+  </div>  
+  @enderror
   
     <input type="text" name="title" class="mt-3 form-control image"
     placeholder="Enter Your Category Title here">
 
-    {{-- <input type="text" name="description" class="mt-3 form-control description"
-    placeholder="Enter  Category description here"> --}}
+    @error('description')
+    <div class="alert alert-danger">
+  
+        {{$message}}
+    </div>  
+    @enderror
 
     <textarea name="description" name="description" class="mt-3 form-control" cols="60" rows="1"
     placeholder="Enter  Category description here"></textarea>
 
     <label class="mt-3 " for="">Enter Your Category Image Here</label>
 
+    @error('image')
+    <div class="alert alert-danger">
+  
+        {{$message}}
+    </div>  
+    @enderror
+
     <input type="file" name="image" class=" form-control">
+
+    @error('link')
+    <div class="alert alert-danger">
+  
+        {{$message}}
+    </div>  
+    @enderror
 
     <input type="text" name="link" class="mt-3 form-control" placeholder="Enter  Category link here" >
 
-     <span class="error_status"></span>
+    @error('status')
+    <div class="alert alert-danger">
+  
+        {{$message}}
+    </div>  
+    @enderror
+
     <select class="mt-3 form-control" name="status">
         <option value="">-----Select Status-----</option>
       
        <option value="1"> Active</option>
        <option value="2"> Inactive</option>
-
-        
        
     </select>
-
-  
 
       <button class="form-control mt-3 btn-purchaseAdd btn btn-success"> Save </button>
 
