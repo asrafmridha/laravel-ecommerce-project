a726df4a103d18f7ffc79336ab463abf63e95688
asraf-mridha <asrafmridha@gmail.com>
2022-09-14 19:24:20 +0600

    for give value in moda button by ajax

--- public/backend/js/slider.js
+++ public/backend/js/slider.js
@@ -19,17 +19,29 @@ jQuery(document).ready(function(){
           <td>'+item.status+'</td>\
           <td><img width="150px" height="150px" src="/backend/slider/'+item.image +'"></td>\
             <td><button id="updatebtn" value="'+item.id+'" class="updateid btn btn-success btn-sm" data-toggle="modal" data-target="#updateModal"><i class="fa fa-edit"></i></button> </td>\
-            <td> <button value="'+item.id+'" class="deleteid btn btn-danger btn-sm" data-toggle="modal" data-target="#deleteModal"><i class="fa fa-trash"></i></button></td>\
+            <td> <button value="'+item.id+'" class="deletesliderid btn btn-danger btn-sm" data-toggle="modal" data-target="#sliderdeltemodal"><i class="fa fa-trash"></i></button></td>\
           </tr>';
           
         });
-        jQuery(".sliderdata").html(data);
-        
-                
+        jQuery(".sliderdata").html(data);        
             }
         });
 
       }
+
+
+      //for give value modal  button
+      jQuery(document).on('click','.deletesliderid',function(){
+       
+        var id=jQuery(this).val();
+        jQuery('.slidermodalbutton').val(id);
+      });
+
+     
+
+
+
+
 
 
 });
