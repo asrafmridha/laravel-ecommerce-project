957a49687ad52af448230ca0f6fc67c08208a150
asraf-mridha <asrafmridha@gmail.com>
2022-09-14 19:15:11 +0600

    Add Bootstarp 4 Modal for Delete Slider

---
+++ resources/views/backend/pages/slider/manageslider.blade.php
@@ -0,0 +1,58 @@
+@extends('mastaring.master')
+ 
+ @section('content')
+
+ <div class="col-md-7">
+
+    <div class="msg">
+        
+    </div>
+    <table class="table">
+       <thead>
+            <tr>
+            <th>Slider Title</th>
+            <th>Category</th>  
+            <th>status</th>
+            <th>Image</th>
+            <th>Action</th>
+            </tr>
+       </thead>
+       <tbody class="sliderdata">
+
+
+            
+       </tbody>
+    </table>
+
+    
+</div>
+
+
+{{-- modal for delte Slider --}}
+<!-- Button trigger modal -->
+ <!-- Modal -->
+ <div class="modal fade" id="sliderdeltemodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+   <div class="modal-dialog" role="document">
+     <div class="modal-content">
+       <div class="modal-header">
+         <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+           <span aria-hidden="true">&times;</span>
+         </button>
+       </div>
+       <div class="modal-body">
+         ...
+       </div>
+       <div class="modal-footer">
+         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+         <button type="button" class="btn btn-primary">Save changes</button>
+       </div>
+     </div>
+   </div>
+ </div>
+
+   
+
+
+
+ @endsection
